c9bb0ebf89b4c292caed16a0dd51a953b440f5c1
shoeb00 <syedshoeb886@gmail.com>
2025-11-09 16:13:23 +0530

    chore: configuration fixes and regenerating migration files

--- apps/backend/drizzle/0000_slim_valeria_richards.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE "users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"email" varchar,
-	"name" varchar NOT NULL,
-	"createdAt" timestamp DEFAULT now(),
-	"updatedAt" timestamp DEFAULT now(),
-	CONSTRAINT "users_email_unique" UNIQUE("email")
-);
