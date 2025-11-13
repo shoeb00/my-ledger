b741d2ead46d0537452c3d27327e75a195f17948
shoeb00 <syedshoeb886@gmail.com>
2025-11-13 13:25:21 +0530

    feat: adding permissions and invitations table

--- apps/api/src/user/schema.ts
+++ apps/api/src/user/schema.ts
@@ -1,9 +1,31 @@
-import { pgTable, serial, varchar, timestamp } from 'drizzle-orm/pg-core';
+import { Roles } from './../permissions/enum/roles';
+import {
+  pgTable,
+  serial,
+  varchar,
+  timestamp,
+  boolean,
+  integer,
+} from 'drizzle-orm/pg-core';
+import { books } from '../book/schema';
+import { roleEnum } from '../permissions/schema';
 
 export const users = pgTable('users', {
   id: serial('id').primaryKey(),
   email: varchar('email').unique().notNull(),
   name: varchar('name').notNull(),
+  createdAt: timestamp('created_at').defaultNow().notNull(),
+  updatedAt: timestamp('updated_at').defaultNow().notNull(),
+});
+
+export const invitations = pgTable('invitations', {
+  id: serial('id').primaryKey(),
+  bookId: integer('book_id')
+    .notNull()
+    .references(() => books.id),
+  email: varchar('email').notNull(),
+  role: roleEnum('role').$type<Roles>().notNull(),
+  accepted: boolean('accepted').default(false).notNull(),
   createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
