de9ed06e777fd65895debe255fa398970b3c29ed
shoeb00 <syedshoeb886@gmail.com>
2025-11-13 13:27:18 +0530

    feat: invite controller and service

--- apps/api/src/user/user.module.ts
+++ apps/api/src/user/user.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { DatabaseModule } from '../database/database.module';
+import { PermissionsService } from '../permissions/permissions.service';
+import { BookService } from '../book/book.service';
 
 @Module({
   imports: [DatabaseModule],
   controllers: [UserController],
-  providers: [UserService],
+  providers: [UserService, PermissionsService, BookService],
 })
 export class UserModule {}
