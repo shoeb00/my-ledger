15e5aedf17937e8e16dc0f7de32f1fd5d2330f7c
shoeb00 <syedshoeb886@gmail.com>
2025-12-21 03:10:04 +0530

    feat: integrating cancel invite action

--- apps/web/app/book/[bookId]/settings/components/invites.tsx
+++ apps/web/app/book/[bookId]/settings/components/invites.tsx
@@ -5,8 +5,24 @@ import { Button } from '@/components/ui/button';
 import { MailPlusIcon, XIcon } from 'lucide-react';
 import type { Invitation } from '../page';
 import { fmtDate } from '../../../../components/book';
+import { cancelInvitation } from '../actions/invitations';
+import { toast } from 'sonner';
 
-export function InvitationRow({ invitation }: { invitation: Invitation }) {
+export function InvitationRow({
+  invitation,
+  refetchAction,
+}: {
+  invitation: Invitation;
+  refetchAction: () => void;
+}) {
+  const handleCancelInvite = async (id: string) => {
+    const { err } = await cancelInvitation(id);
+    if (err) toast.error(err);
+    else {
+      toast.success('Invitation canceled successfully');
+      refetchAction();
+    }
+  };
   return (
     <article
       role="listitem"
@@ -18,18 +34,24 @@ export function InvitationRow({ invitation }: { invitation: Invitation }) {
 
       <Badge className="text-sm capitalize shrink-0 w-[120px]">{invitation.role}</Badge>
 
-      <Button variant="ghost">
+      <Button variant="ghost" onClick={() => handleCancelInvite(invitation.id.toString())}>
         <XIcon />
       </Button>
     </article>
   );
 }
 
-export default function InvitationList({ invitations }: { invitations: Invitation[] }) {
+export default function InvitationList({
+  invitations,
+  refetchAction,
+}: {
+  invitations: Invitation[];
+  refetchAction: () => void;
+}) {
   if (!invitations || invitations.length === 0) {
     return (
       <div className="rounded-md border border-dashed h-85 p-6 text-center text-sm text-muted-foreground">
-        <div className="mb-2 font-medium">No invitations yet</div>
+        <div className="my-2 font-medium">No invitations yet</div>
         <div className="mb-4 text-xs">Add a invitation to see them listed here.</div>
         <Button>
           <MailPlusIcon /> Send Invitation
@@ -41,7 +63,7 @@ export default function InvitationList({ invitations }: { invitations: Invitatio
   return (
     <section className="h-105 overflow-auto space-y-2">
       {invitations.map(m => (
-        <InvitationRow invitation={m} key={m.email} />
+        <InvitationRow invitation={m} key={m.email} refetchAction={refetchAction} />
       ))}
     </section>
   );
