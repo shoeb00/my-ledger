deec15394e6436d36952e164d7139f27d80765d2
shoeb00 <syedshoeb886@gmail.com>
2025-12-30 21:21:30 +0530

    chore: refactor layout file to segregate client components and adding .env to gitignore

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import './globals.css';
-import { ClerkProvider } from '@clerk/nextjs';
-import Navbar from './components/navbar';
-import { Toaster } from '@/components/ui/sonner';
+import Providers from './providers';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -27,13 +25,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ClerkProvider>
-          <Toaster className="top-0" position="top-right" visibleToasts={5} />
-          <Navbar />
-          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6">
-            {children}
-          </div>
-        </ClerkProvider>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
