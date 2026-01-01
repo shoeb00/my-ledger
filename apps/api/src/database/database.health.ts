e68ebd07244ac927e4a6142a93cfcbf0eb65dd12
shoeb00 <syedshoeb886@gmail.com>
2026-01-02 02:59:13 +0530

    feat: creating database healthCheck and init services

---
+++ apps/api/src/database/database.health.ts
@@ -0,0 +1,20 @@
+import { Inject, Injectable } from '@nestjs/common';
+import { DATABASE_POOL } from './database-connection';
+import { Pool } from 'pg';
+
+@Injectable()
+export class DatabaseHealthService {
+  constructor(
+    @Inject(DATABASE_POOL)
+    private readonly db_pool: Pool,
+  ) {}
+
+  async check() {
+    try {
+      await this.db_pool.query('SELECT 1');
+      return { status: 'up' } as const;
+    } catch {
+      return { status: 'down' } as const;
+    }
+  }
+}
