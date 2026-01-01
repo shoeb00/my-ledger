b3a12d21032da13f1af9ac6e1a13b6583180965e
shoeb00 <syedshoeb886@gmail.com>
2026-01-02 04:07:35 +0530

    fix: using url for drizzle config

--- apps/api/drizzle.config.ts
+++ apps/api/drizzle.config.ts
@@ -6,11 +6,6 @@ export default defineConfig({
   out: './drizzle',
   dialect: 'postgresql',
   dbCredentials: {
-    host: process.env.DATABASE_HOST || 'localhost',
-    port: Number(process.env.DATABASE_PORT),
-    user: process.env.DATABASE_USER || 'postgres',
-    password: process.env.DATABASE_PASSWORD || 'password',
-    database: process.env.DATABASE_NAME || 'my_database',
-    ssl: process.env.DATABASE_SSL === 'true',
+    url: process.env.DATABASE_URL!,
   },
 });
