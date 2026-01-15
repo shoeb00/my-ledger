698e63b8b6bbba2e603c7bc386080ff7a46d973f
shoeb00 <syedshoeb886@gmail.com>
2026-01-16 03:51:25 +0530

    chore: fixing the book response interface

--- apps/web/app/book/actions/get-books.ts
+++ apps/web/app/book/actions/get-books.ts
@@ -1,8 +1,10 @@
 import { Book } from '@my-ledger/api/book';
 import { callApi } from '../../lib/api';
+import { Roles } from '@my-ledger/api/role';
 
 export interface BookResponse extends Book {
   lastTransaction: string;
+  role: Roles;
 }
 export async function getBook(bookId?: string) {
   const endpoint = '/v1/book/get';
