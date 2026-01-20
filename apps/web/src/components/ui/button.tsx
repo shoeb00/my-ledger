80ee40ef1a1cb60c165af41be814b239657815c4
shoeb00 <syedshoeb886@gmail.com>
2026-01-20 13:55:42 +0530

    feat: adding drop zone for file upload

--- apps/web/src/components/ui/button.tsx
+++ apps/web/src/components/ui/button.tsx
@@ -38,8 +38,8 @@ const buttonVariants = cva(
 
 function Button({
   className,
-  variant,
-  size,
+  variant = "default",
+  size = "default",
   asChild = false,
   ...props
 }: React.ComponentProps<"button"> &
@@ -51,10 +51,15 @@ function Button({
   return (
     <Comp
       data-slot="button"
+      data-variant={variant}
+      data-size={size}
       className={cn(buttonVariants({ variant, size, className }))}
       {...props}
     />
   )
 }
+
+export type ButtonProps = React.ComponentProps<typeof Button>;
+
 
 export { Button, buttonVariants }
