39f64af9df50c93dd1315098fc441d3befaef020
shoeb00 <syedshoeb886@gmail.com>
2026-01-20 13:57:27 +0530

    feat: adding bulkCreate transaction action

--- apps/web/app/book/[bookId]/actions/add-transaction.ts
+++ apps/web/app/book/[bookId]/actions/add-transaction.ts
@@ -1,7 +1,18 @@
 import { callApi } from '../../../lib/api';
+import { Transaction } from '@my-ledger/api/transaction';
 import { AddRequestPayload } from '../interfaces/add-request-payload';
 
-export async function addTransaction(payload: AddRequestPayload) {
+export async function addTransaction(payload: AddRequestPayload, bookId: string) {
   const endpoint = '/v1/transaction/create';
-  return await callApi(endpoint, 'POST', undefined, payload);
+  return await callApi(endpoint, 'POST', { bookId }, payload);
+}
+
+export type AddTransactionRequest = Omit<Transaction, 'id' | 'updatedAt' | 'userId' | 'bookId'>;
+
+export async function bulkAddTransaction(
+  payload: { transactions: AddTransactionRequest[] },
+  bookId: string,
+) {
+  const endpoint = '/v1/transaction/createBulk';
+  return await callApi(endpoint, 'POST', { bookId }, payload);
 }
