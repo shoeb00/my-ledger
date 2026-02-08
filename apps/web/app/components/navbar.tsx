fa5d1c1d0f2dd660ac0a3ddaa519f18389276623
shoeb00 <syedshoeb886@gmail.com>
2026-02-08 20:58:11 +0530

    feat: adding logo on the navbar

--- apps/web/app/components/navbar.tsx
+++ apps/web/app/components/navbar.tsx
@@ -2,29 +2,46 @@
 import { Button } from '@/components/ui/button';
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from '@clerk/nextjs';
 import { MoonIcon, SunDimIcon } from 'lucide-react';
+import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 export default function Navbar({ theme, setThemeAction }: { theme: boolean, setThemeAction: () => void }) {
+  const router = useRouter();
   return (
-    <header className="flex justify-end items-center p-4 gap-4 h-16">
-      <Button variant="ghost" onClick={setThemeAction}> {theme ? <SunDimIcon /> : <MoonIcon />}</Button>
-      <SignedOut>
-        <SignInButton mode="modal">
-          <Button>Sign In</Button>
-        </SignInButton>
-        <SignUpButton mode="modal">
-          <Button variant="outline"> Create Account </Button>
-        </SignUpButton>
-      </SignedOut>
-
-      <SignedIn>
-        <UserButton showName appearance={{
-          elements: {
-            userButtonBox: {
-              textTransform: 'capitalize',
+    <header className="flex flex-row justify-between items-center p-4 gap-4 h-16">
+      <div>
+        <Button variant='ghost' onClick={() => router.push('/home')} className='hover:bg-transparent h-25 p-0'>
+          {/* TODO: Handle dark mode for the logo */}
+          <Image
+            src="/logo.svg"
+            alt="My Ledger Logo"
+            width={30}
+            height={20}
+            className=''
+          />
+          <span>My Ledger</span>
+        </Button>
+      </div>
+      <div className="flex flex-row items-center gap-4">
+        <Button variant="ghost" onClick={setThemeAction}> {theme ? <SunDimIcon /> : <MoonIcon />}</Button>
+        <SignedIn>
+          <UserButton showName appearance={{
+            elements: {
+              userButtonBox: {
+                textTransform: 'capitalize',
+              },
             },
-          },
-        }} />
-      </SignedIn>
-    </header>
+          }} />
+        </SignedIn>
+        <SignedOut>
+          <SignInButton mode="modal">
+            <Button>Sign In</Button>
+          </SignInButton>
+          <SignUpButton mode="modal">
+            <Button variant="outline"> Create Account </Button>
+          </SignUpButton>
+        </SignedOut>
+      </div>
+    </header >
   );
 }
