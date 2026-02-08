b4d23d2f351e526ea60fb1b98f5d3be1cd44a368
shoeb00 <syedshoeb886@gmail.com>
2026-02-08 20:55:01 +0530

    feat: adding metadata and making layout.tsx a server component

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,8 +1,7 @@
-'use client';
 import localFont from 'next/font/local';
 import './globals.css';
 import Providers from './providers';
-import { useState } from 'react';
+import { Metadata } from 'next';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -13,16 +12,49 @@ const geistMono = localFont({
   variable: '--font-geist-mono',
 });
 
+export const metadata: Metadata = {
+  title: {
+    default: 'My Ledger',
+    template: '%s | My Ledger',
+  },
+  description: 'A simpler, free alternative to cashbook.in',
+  icons: {
+    icon: [
+      { url: '/favicon.ico' },
+      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+    ],
+    apple: '/apple-touch-icon.png',
+  },
+  metadataBase: new URL('https://my-ledger.app'),
+  openGraph: {
+    title: 'My Ledger',
+    description: 'Move your ledger in minutes. Clean, free, private.',
+    url: 'https://my-ledger.app',
+    siteName: 'My Ledger',
+    images: [
+      {
+        url: '/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'My Ledger',
+      },
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+};
+
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [theme, setTheme] = useState(false);
   return (
-    <html lang="en" className={theme ? 'dark' : ''}>
+    <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <Providers theme={theme} setThemeAction={() => setTheme(!theme)}>{children}</Providers>
+        <Providers>{children}</Providers>
       </body>
     </html>
   );
