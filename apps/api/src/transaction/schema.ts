066098bf0b005348b86b107d9be49f06c16dc4a0
shoeb00 <syedshoeb886@gmail.com>
2026-02-13 00:25:58 +0530

    feat: updating schema files and importing in database module

--- apps/api/src/transaction/schema.ts
+++ apps/api/src/transaction/schema.ts
@@ -8,6 +8,8 @@ import {
 } from 'drizzle-orm/pg-core';
 import { books } from '../book/schema';
 import { users } from '../user/schema';
+import { paymentMethods } from '../payment-method/schema';
+import { categories } from '../category/schema';
 import { InferSelectModel } from 'drizzle-orm';
 
 export const transactions = pgTable('transactions', {
@@ -20,8 +22,10 @@ export const transactions = pgTable('transactions', {
     .references(() => users.id),
   amount: numeric('amount', { precision: 10, scale: 2 }).notNull(),
   description: varchar('description'),
-  paymentType: varchar('payment_type'),
-  category: varchar('category'),
+  paymentMethodId: integer('payment_method_id').references(
+    () => paymentMethods.id,
+  ),
+  categoryId: integer('category_id').references(() => categories.id),
   createdAt: timestamp('created_at').defaultNow().notNull(),
   updatedAt: timestamp('updated_at').defaultNow().notNull(),
 });
