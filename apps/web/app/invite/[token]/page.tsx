8e972e8ede28471b815dc3de614549266035287b
shoeb00 <syedshoeb886@gmail.com>
2026-02-26 09:12:59 +0530

    fix: moving generateMetadata() to a server module

--- apps/web/app/invite/[token]/page.tsx
+++ apps/web/app/invite/[token]/page.tsx
@@ -4,32 +4,6 @@ import { useEffect } from 'react';
 import { toast } from 'sonner';
 import LoaderCircle from '../../components/loader';
 import { useUser } from '@clerk/nextjs';
-import type { Metadata } from 'next';
-
-export async function generateMetadata({ params }: { params: { token: string } }): Promise<Metadata> {
-    const baseUrl = process.env.WEB_URL!;
-    const { token } = params;
-
-    return {
-        openGraph: {
-            title: 'My Ledger – Invitation',
-            description: 'Join My Ledger and manage credits & debits seamlessly.',
-            url: `${baseUrl}/invite/${token}`,
-            images: [
-                {
-                    url: `${baseUrl}/og-image.png`,
-                    width: 1200,
-                    height: 630,
-                },
-            ],
-        },
-        twitter: {
-            card: 'summary_large_image',
-            images: [`${baseUrl}/og-image.png`],
-        },
-    };
-}
-
 
 export default function AcceptInvite() {
     const router = useRouter();
