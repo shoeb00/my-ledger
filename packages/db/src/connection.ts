eadd7edf963cff0be83f2face33344d6f79a2597
shoeb00 <syedshoeb886@gmail.com>
2026-05-08 13:42:15 +0530

    fix: db import type

--- packages/db/src/connection.ts
+++ packages/db/src/connection.ts
@@ -1,4 +1,4 @@
-import { drizzle } from "drizzle-orm/node-postgres"
+import { drizzle, type NodePgDatabase } from "drizzle-orm/node-postgres"
 import { Pool } from "pg"
 import * as schema from './schema/index.js'
 
@@ -6,4 +6,4 @@ export const createDb = (pool: Pool) => {
     return drizzle(pool, { schema, casing: 'snake_case' })
 }
 
-export type DB = ReturnType<typeof createDb>;
+export type DB = NodePgDatabase<typeof schema> & { $client: Pool };
